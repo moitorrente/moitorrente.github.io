99d299c269f1be878be50ebd3b6d77192ecc112d
moitorrente <moi.torrente@gmail.com>
2017-10-08 22:38:08 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,14 +12,13 @@ function draw() {
 	stroke(255);
 
 	if (random(1)>0.5){
-	line (x, y, x+spacing, y+spacing)
-}else{
+		line (x, y, x+spacing, y+spacing)
+	}else{
 	line (x, y+spacing, x+spacing, y)
-}
+	}
 	x = x+spacing;
 	if (x>width){
 		x = 0;
 		y = y + spacing
-
 	}
 }
